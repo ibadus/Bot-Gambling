07aa971d1bac12da385b9b1d18db49586d4eb261
Oddon <ibadus@icloud.com>
2020-04-12 17:39:15 +0200

    0.02

--- CSGO-Empire-Bot.py
+++ CSGO-Empire-Bot.py
@@ -1,4 +1,3 @@
-import time
 import keyboard
 import requests
 import selenium
@@ -9,119 +8,107 @@ from selenium.webdriver import DesiredCapabilities
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver import ActionChains
+import time
+import datetime
 
 # Fonctions
-def TERRO(browser, mise):
-    bet = browser.find_element_by_xpath("/html/body/div[1]/div[1]/div[2]/div/div/div/section/div/div/div[5]/div/div[1]/input")
-    bet.send_keys(mise) #mise la mise #PAS bmise
-    cote = browser.find_element_by_xpath("/html/body/div[1]/div[1]/div[2]/div/div/div/section/div/div/div[7]/div[3]/div/div/button/div/div[1]/div/span")
-    cote.click()
-
-def CT(browser, mise):
-    bet = browser.find_element_by_xpath("/html/body/div[1]/div[1]/div[2]/div/div/div/section/div/div/div[5]/div/div[1]/input")
-    bet.send_keys(mise) #mise la mise #PAS bmise
-    cote = browser.find_element_by_xpath("/html/body/div[1]/div[1]/div[2]/div/div/div/section/div/div/div[7]/div[1]/div/div/button/div/div[1]/div/span")
-    cote.click()
-
-def win(mise, bmise):
-    mise = bmise
-
-def loose(mise):
-    mise = mise * 2
+def TERRO(browser,mise,i,retrait,start,timer,bmise):
+    while i < retrait :
+        balance = browser.find_element_by_xpath("/html/body/div[1]/div[1]/div[1]/div[1]/div/div[3]/div[2]/div[3]/div/div/span/span").text
+        print("Vous avez {} dans votre balance".format(balance))
+        time = True
+        while time :
+            timer = browser.find_element_by_xpath("/html/body/div[1]/div[1]/div[2]/div/div/div/section/div/div/div[3]/div[2]/div/div[2]").text
+            if str(timer) != "15,00":
+                time = False
+            elif str(timer) != "15,01":
+                time = False
+            elif str(timer) != "15,02":
+                time = False
+            print(timer)
+        print("===========================")
+        print("Betting...")
+        print("===========================")
+        bet = browser.find_element_by_xpath("/html/body/div[1]/div[1]/div[2]/div/div/div/section/div/div/div[5]/div/div[1]/input")
+        bet.send_keys(Keys.CONTROL + "a")
+        bet.send_keys(Keys.DELETE)
+        mise = str(mise)
+        bet.send_keys(mise) #bet de la mise
+        print("2s...")
+        browser.implicitly_wait(2)
+        cote = browser.find_element_by_xpath("/html/body/div[1]/div[1]/div[2]/div/div/div/section/div/div/div[6]/div[3]/div/div/button/div/div[1]/div/span")
+        cote.click()
+        print("===========================")
+        print("Bet fait ! ")
+        print("===========================")
+        print("En attenete de la nouvelle partie...")
+        print("===========================")
+        browser.implicitly_wait(12)
+        start = ""
+        while str(start) != "0,00":
+            start = browser.find_element_by_xpath("/html/body/div[1]/div[1]/div[2]/div/div/div/section/div/div/div[7]/div[3]/div/div[1]/div[2]/span").text
+        newbalance = browser.find_element_by_xpath("/html/body/div[1]/div[1]/div[1]/div[1]/div/div[3]/div[2]/div[3]/div/div/span/span").text
+        if newbalance > balance :
+            mise = bmise
+            print("W")
+        elif newbalance < balance :
+            mise = float(mise)
+            mise = mise * 2
+            i = i + 1
+            print("L")
+        else :
+            browser.implicitly_wait(6)
+            newbalance = browser.find_element_by_xpath("/html/body/div[1]/div[1]/div[1]/div[1]/div/div[3]/div[2]/div[3]/div/div/span/span").text
+            print("Else")
+            if newbalance > balance :
+                mise = bmise
+                print("W")
+            elif newbalance < balance :
+                mise = float(mise)
+                mise = mise * 2
+                i = i + 1
+                print("L")
+        print("===========================")
+        print("Nouvelle mise = {}".format(mise))
+        print("===========================")
+        print("Nouvelle partie")
+        print("===========================")
+        balance = newbalance
 
 # Inputs
-bmise = str(input("Entrez la mise voulue : "))
-couleur = str(input("CT ou TERRO : "))
-retrait = int(input("Nombre de lose de suite maximum : "))
-userpin = str(input("Entrez votre PIN : "))
-
-# Annonce des règles
-print("Vous avez choisis {} comme mise de base qui sera *2 lorsque vous perderez.\nLe bot s'arrêtera lorsque vous aurez fait {} défaites de suites.\nVous miserez uniquement sur le côté {}.".format(bmise,retrait,couleur))
+bmise = ##################################### A CHANGER #####################################
+couleur = "TERRO"
+retrait = ##################################### A CHANGER ##################################### 
 
 # Variables
 url = "https://csgoempire.com/"
 mise = bmise
 i = 0
-timer = 0
+timer = "1"
 start = 1
+a = True
 
 # Début du programme
     # Ouverture du navigateur
         # Option = ouvrir le browser sur l'utilisateur actuel
 chrome_options = Options()
-chrome_options.add_argument('--user-data-dir=C:\\Users\\VOTRE NOM D UTILISATEUR\\AppData\\Local\Google\\Chrome\\User Data') ##################################### A CHANGER ##################################### 
+chrome_options.add_argument('--user-data-dir=C:\\Users\\Oddon\\AppData\\Local\Google\\Chrome\\User Data') ##################################### A CHANGER ##################################### 
 capabilities = DesiredCapabilities.CHROME.copy()
-chromedriver = "C:\\Users\\VOTRE NOM D UTILISATEUR\\chromedriver.exe" ##################################### A CHANGER ##################################### 
+chromedriver = "C:\\Users\\Oddon\\chromedriver.exe" ##################################### A CHANGER ##################################### 
 browser = webdriver.Chrome(executable_path=chromedriver, chrome_options = chrome_options, desired_capabilities=capabilities) # Ouverture de Chrome
 browser.get(url) # Ouvre l'URL
 
-    # Lancement du programme
-browser.implicitly_wait(5) # Browser attends 5 secondes
-pin = browser.find_element_by_xpath("/html/body/div[1]/div[1]/div[2]/div/div/div/section/div/div/div[5]/div/div[1]/div[1]/div/div/div/input")
-pin.send_keys(userpin) # Brwoser entre le PIN
-confirm = browser.find_element_by_xpath("/html/body/div[1]/div[1]/div[2]/div/div/div/section/div/div/div[5]/div/div[1]/div[1]/div/div/div/button")
-confirm.click() # Browser clique sur 'Confirm'
-
-print("===========================\n'A' pour LANCER le programme sur l'invite de commande.")
-print("===========================\nESC pour QUITTER le programme sur l'invite de commande.\n===========================")
-
-time.sleep(5)
-balance = browser.find_element_by_xpath("/html/body/div[1]/div[1]/div[1]/div[2]/div/div[1]/div/div/span/span").text
-print("Vous avez {} dans votre balance".format(balance))
-print("===========================")
-keyboard.wait('a')
-if couleur == "TERRO":
-    while i < retrait :
-        while str(timer) != "15,00":
-            timer = browser.find_element_by_xpath("/html/body/div[1]/div[1]/div[2]/div/div/div/section/div/div/div[4]/div[2]/div/div[2]").text
-            if keyboard.read_key() == 'esc':
-                print("Fermeture du programme...")
-                browser.quit()
-                quit()
-        newbalance = browser.find_element_by_xpath("/html/body/div[1]/div[1]/div[1]/div[2]/div/div[1]/div/div/span/span").text
-        TERRO(browser, mise)
-        while str(start) != "0,00":
-            start = browser.find_element_by_xpath("/html/body/div[1]/div[1]/div[2]/div/div/div/section/div/div/div[7]/div[2]/div/div[1]/div[2]/span").text
-            if keyboard.read_key() == 'esc':
-                print("Fermeture du programme...")
-                browser.quit()
-                quit()
-        if newbalance > balance :
-            win(mise, bmise)
-        elif newbalance < balance :
-            loose(mise)
-            i = i + 1
-        elif keyboard.read_key() == 'esc':
-            print("Fermeture du programme...")
-            browser.quit()
-            quit()
-        print("New game....")
-        balance = newbalance
-
-if couleur == "CT":
-    while i < retrait :
-        while str(timer) != "15,00":
-            timer = browser.find_element_by_xpath("/html/body/div[1]/div[1]/div[2]/div/div/div/section/div/div/div[4]/div[2]/div/div[2]").text
-            if keyboard.read_key() == 'esc':
-                print("Fermeture du programme...")
-                browser.quit()
-                quit()
-        newbalance = browser.find_element_by_xpath("/html/body/div[1]/div[1]/div[1]/div[2]/div/div[1]/div/div/span/span").text
-        CT(browser, mise)
-        while str(start) != "0,00":
-            start = browser.find_element_by_xpath("/html/body/div[1]/div[1]/div[2]/div/div/div/section/div/div/div[7]/div[2]/div/div[1]/div[2]/span").text
-            if keyboard.read_key() == 'esc':
-                print("Fermeture du programme...")
-                browser.quit()
-                quit()
-        if newbalance > balance :
-            win(mise, bmise)
-        elif newbalance < balance :
-            loose(mise)
-            i = i + 1
-        elif keyboard.read_key() == 'esc':
-            print("Fermeture du programme...")
-            browser.quit()
-            quit()
-        print("New game....")
-        balance = newbalance
+print("Vous avez choisis {} comme mise de base qui sera *2 lorsque vous perderez.\nLe bot s'arrêtera lorsque vous aurez fait {} défaites de suites.\nVous miserez uniquement sur le côté {}.".format(bmise,retrait,couleur))
+print("===========================\n'ESC' pour LANCER le bot sur Chrome.\n===========================")
+while a :
+    if keyboard.read_key() == 'esc':
+        a = False
+        TERRO(browser,mise,i,retrait,start,timer,bmise)
+    #if keyboard.read_key() == 'esc':
+        # Lancement du programme 
+        #browser.implicitly_wait(5) # Browser attends 5 secondes
+        #pin = browser.find_element_by_xpath("/html/body/div[@id='app']/div[@class='site-layout']/div[@class='site-layout__main relative z-10']/div[@class='page-layout relative z-10']/div[@id='page-scroll']/div[@class='page-layout__inner']/section[@class='w-full']/div[@class='page']/div[@class='page__inner w-full mx-auto pt-8 lg:pt-12 pb-8']/div[@class='bet-input lg:flex lg:justify-center mb-4 lg:mb-5']/div[@class='lg:inline-flex']/div[@class='bet-input__field relative']/div[@class='bet-input__pin']/div/div[@class='input-form mx-auto']/div[@class='relative']/input[@class='security-prompt--roulette']")
+        #pin.send_keys(userpin) # Brwoser entre le PIN
+        #confirm = browser.find_element_by_xpath("/html/body/div[@id='app']/div[@class='site-layout']/div[@class='site-layout__main relative z-10']/div[@class='page-layout relative z-10']/div[@id='page-scroll']/div[@class='page-layout__inner']/section[@class='w-full']/div[@class='page']/div[@class='page__inner w-full mx-auto pt-8 lg:pt-12 pb-8']/div[@class='bet-input lg:flex lg:justify-center mb-4 lg:mb-5']/div[@class='lg:inline-flex']/div[@class='bet-input__field relative']/div[@class='bet-input__pin']/div/div[@class='input-form mx-auto']/div[@class='relative']/button[@class='absolute pin-r pin-t px-2 mt-03 mr-03 button-primary button-primary--gold']")
+        #confirm.click() # Browser clique sur 'Confirm'
+        # Annonce des règles
--- README.md
+++ README.md
@@ -6,12 +6,11 @@ Ceci est un bot de gambling. Si vous avez des questions, contactez moi sur disco
 
 1) Lorsque vous lancez le programme verifiez bien que le bot se lance correctement. Si une page de dialogue apparait, le bot attendra que cette fenetre de dialogue se ferme.
 2) Appuyez sur 'a' pour lancer le bot après avoir entrer vos données, le bot se lancera 5 secondes après avoir appuyé sur 'a' donc prenez ce temps en compte.
-3) Appuyez sur 'esc' en étant sur la CMD pour quitter le bot.
+3) Appuyez sur 'esc' en étant sur la CMD pour lancer le bot.
 4) Vous pouvez fermer la CMD pour quitter le programme, ceci fermera la page internet.
 5) IL EST ABSOLUMENT NECESSAIRE de fermer toutes ses fenêtres Chrome avant de lancer le bot. Vous pourrez les reouvrir après.
-6) IL EST ABSOLUMENT NECESSAIRE d'être connecté avec son compte Steam sur CSGO-Empire lorsqu'on lance le bot.
-7) IL EST ABSOLUMENT NECESSAIRE d'avoir renseigné son adresse et toutes ses infos pour ne pas avoir la pop up avant de lancer le bot.
-8) IL EST ABSOLUMENT NECESSAIRE d'avoir installé les prérequis.
+6) IL EST ABSOLUMENT NECESSAIRE d'avoir renseigné son adresse et toutes ses infos pour ne pas avoir la pop up avant de lancer le bot.
+7) IL EST ABSOLUMENT NECESSAIRE d'avoir installé les prérequis.
 
 # --- Prérequis ---
 
@@ -20,7 +19,7 @@ Ceci est un bot de gambling. Si vous avez des questions, contactez moi sur disco
 2) Installer 'pip' :
 2.1) Ouvrir l'invite de commande (CMD) et écrire "python get-pip.py".
 3) Installer toutes les bibliothèques :
-3.1) Ecrire dans la CMD "pip3 install keyboard requests selenium".
+3.1) Ecrire dans la CMD "pip3 install keyboard requests selenium datetime".
 4) Installer ChromeDriver :
 4.1) Ouvrez votre chrome. Cliquer sur les 'trois petits points' en haut a droite.Cliquer sur 'aide'. Et cliquer sur 'A propos de Google Chrome'.
 4.2) Noter votre version de Chrome.
@@ -31,18 +30,19 @@ Ceci est un bot de gambling. Si vous avez des questions, contactez moi sur disco
 
 1) Installer vous aurez à télécharger 'CSGO-Empire-Bot.py'.
 2) Déplacer 'CSGO-Empire-Bot.py' sur votre bureau.
-3) Ouvrir 'CSGO-Empire-Bot.py' avec un éditeur de texte tel que sublime texte. 
-4) Chercher les "##################################### A CHANGER #####################################" dans le code. 
-4.1) Modifier dans le chemin d'accès "VOTRE NOM D UTILISATEUR" par votre nom d'utilisateur. Si cela ne fonctionne pas, essayez de faire le même chemin que indiqué et de ne pas oublier de mettre des // à la place des /. NE PAS TOUCHER A "--user-data-dir=" et écrire son path après !
-4.2) Sauvgarder avec 'ctrl+S'.
+3) Ouvrir 'CSGO-Empire-Bot.py' avec un éditeur de texte tel que sublime texte.
+4) Chercher les "##################################### A CHANGER #####################################" dans le code.
+4.1) Vous tomberez en premier sur 'bmise = '. Après le '=' entrez la valeur de base par exemple : bmise = 0.01
+4.2) Ensuite 'retrait'. Après le '=' entrez le nombre de L max, par exemple : retrait = 6
+4.3) Modifier dans le chemin d'accès "VOTRE NOM D UTILISATEUR" par votre nom d'utilisateur. Si cela ne fonctionne pas, essayez de faire le même chemin qu'indiqué et de ne pas oublier de mettre des // à la place des /. NE PAS TOUCHER A "--user-data-dir=" et écrire son path après !
+4.4) Sauvgarder avec 'ctrl+S'.
 5) Exectuer le programme 'CSGO-Empire-Bot.py'.
 5.1) Copiez votre fichier 'CSGO-Empire-Bot.py' en faisant clique droit dessus et 'copier'.
-5.2) Ouvrez votre cmd et écrire "Python3 (ctrl+v)" (ctrl+v = le chemin du fichier ).
+5.2) Ouvrez votre cmd et écrire "pyhton3 " et ajouter ensuite le chemin de 'CSGO-Empire-Bot.py' que vous venez de copier.
 5.3) Appuyez sur 'Entrée'.
 
 # --- Informations ---
 
-"Entrez la mise voulue : " = Ce sera la mise de base que le bot entrera à chaque fois. 
+"Entrez la mise voulue : " = Ce sera la mise de base que le bot entrera à chaque fois.
 "CT ou TERRO : " = Couleur sur laquelle vous voulez que le bot mise à chaques fois.
 "Nombre de lose de suite maximum : " = Nombre de perte de suite maximum où le bot s'arrêtera.
-"Entrez votre PIN : " = Votre PIN pour dévérouiller le compte et laisser le bot tout faire.
